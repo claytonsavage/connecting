f1c5f41cde418b689cb7e8fa3827a49691d294e0
claytonsavage <clytnsavage@gmail.com>
2017-12-19 21:43:37 +0000

    updated some refactoring on argv_script

--- argv_script.js
+++ argv_script.js
@@ -1,7 +1,6 @@
 const pg = require("pg");
 const settings = require('./settings');
 const client = require('./client.js');
-
 const userInput = process.argv[2];
 const resultPrinter = (result) => {console.log(result.rows);}
 
@@ -9,12 +8,13 @@ client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
-  client.query("SELECT * FROM famous_people WHERE last_name = $1",[userInput],(err, result) => {
+  clientQuery
+});
+
+let clientQuery = client.query("SELECT * FROM famous_people WHERE last_name = $1",[userInput],(err, result) => {
     if (err) {
       return console.error("Error running query", err);
     }
     resultPrinter(result);
     client.end();
   });
-});
-
