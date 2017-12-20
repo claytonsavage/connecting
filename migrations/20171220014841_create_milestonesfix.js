cee2a677ab27cca7adb5da4aeb3c45548587d5b7
claytonsavage <clytnsavage@gmail.com>
2017-12-20 02:06:01 +0000

    added the foreign key of famous people to the milestones table

---
+++ migrations/20171220014841_create_milestonesfix.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('milestones', function(table){
+      table.integer('people_id');
+      table.foreign('people_id').references('famous_people.id');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('milestones', function(table) {
+      table.dropColumn('people_id');
+    })
+  ])
+};
