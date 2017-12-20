69423fe27d5abe3a22a97300a3904c8e61659f32
claytonsavage <clytnsavage@gmail.com>
2017-12-20 01:39:33 +0000

    added the first milestone table via migration

---
+++ knexfile.js
@@ -0,0 +1,22 @@
+const settings = require('./settings');
+const client = require('./client.js');
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: settings.database,
+      user:     settings.user,
+      password: settings.password
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
